072e478d3d9ea9a004534cdcfa954378b8f94259
djeurvaisse <djeurvaisse@gmail.com>
2018-01-30 14:33:04 -0500

    updated repo test

--- .storybook/config.js
+++
@@ -1,7 +0,0 @@
-import {configure} from '@storybook/react';
-
-function loadStories() {
-  require('../src/components/ActionButton/__stories__/index.js');
-}
-
-configure(loadStories, module);
--- src/utils/index.js
+++
@@ -1,2 +0,0 @@
-export {default as colors} from './styles/colors';
-export {default as fonts} from './styles/fonts';
