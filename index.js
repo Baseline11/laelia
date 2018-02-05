52ef9ad7fe1bc0270edfd346fe44e9cac31e38c3
djeurvaisse <djeurvaisse@gmail.com>
2018-02-05 10:12:33 -0500

    added common export for new Table component and its subcomponent

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 export {default as ActionButton} from './components/ActionButton';
+export {default as Table} from './components/Table';
+export {default as TableHeader} from './components/TableHeader';
+export {default as TableCell} from './components/TableCell';
