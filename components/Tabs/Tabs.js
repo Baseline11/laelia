4417aca99dff7da615538551c91d70fdfe560b15
jenkins <jenkins@baselinetelematics.com>
2018-05-23 10:59:24 -0400

    Packaging changes from Lealia commit https://bitbucket.baseline.info/projects/FC/repos/laelia/commits/6a31d7118c3e226bafb37475ccde614cca38d84d

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -22,23 +22,28 @@ var _styles2 = _interopRequireDefault(_styles);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function MenuPage(_ref) {
+function Tab(_ref) {
   var handleOnClick = _ref.handleOnClick,
       tabs = _ref.tabs,
       selectedTab = _ref.selectedTab,
       containerStyleOverride = _ref.containerStyleOverride,
-      tabStyleOverride = _ref.tabStyleOverride;
+      tabStyleOverride = _ref.tabStyleOverride,
+      type = _ref.type;
+
+  var wrapperStyleClass = type === 'secondaryStyle' ? _styles2.default.secondaryTabWrapper : _styles2.default.tabWrapper;
+  var styleClass = type === 'secondaryStyle' ? _styles2.default.tabSecondaryStyle : _styles2.default.tabStyle;
+  var styleSelectedClass = type === 'secondaryStyle' ? _styles2.default.tabSecondaryStyleSelected : _styles2.default.tabStyleSelected;
 
   return _react2.default.createElement(
     'div',
-    { style: [_styles2.default.tabWrapper, containerStyleOverride] },
+    { style: [wrapperStyleClass, containerStyleOverride] },
     tabs.map(function (tab, index) {
       return _react2.default.createElement(
         'button',
         {
           key: 'button-' + index,
           onClick: handleOnClick,
-          style: [_styles2.default.tabStyle, selectedTab === tab && _styles2.default.tabStyleSelected, tabStyleOverride]
+          style: [styleClass, selectedTab === tab && styleSelectedClass, tabStyleOverride]
         },
         tab
       );
@@ -46,12 +51,13 @@ function MenuPage(_ref) {
   );
 }
 
-MenuPage.propTypes = {
+Tab.propTypes = {
   handleOnClick: _propTypes2.default.func.isRequired,
   selectedTab: _propTypes2.default.string,
   tabs: _propTypes2.default.array.isRequired,
-  containerStyleOverride: _propTypes2.default.array.object,
-  tabStyleOverride: _propTypes2.default.array.object
+  containerStyleOverride: _propTypes2.default.object,
+  tabStyleOverride: _propTypes2.default.object,
+  type: _propTypes2.default.string
 };
 
-exports.default = (0, _radium2.default)(MenuPage);
+exports.default = (0, _radium2.default)(Tab);
--- components/Tabs/styles.js
+++ components/Tabs/styles.js
@@ -4,16 +4,18 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
 var _utils = require('../../utils');
 
 exports.default = {
-  tabWrapper: {
+  tabWrapper: _extends({}, _utils.fonts.baseText, {
     display: 'flex',
     justifyContent: 'flex-start',
     alignItems: 'stretch',
     height: 100,
     borderBottom: '2px solid ' + _utils.colors.silver
-  },
+  }),
   tabStyle: {
     flex: 1,
     height: '100%',
@@ -35,5 +37,44 @@ exports.default = {
   tabStyleSelected: {
     color: _utils.colors.pictonBlue,
     borderTop: '2px solid ' + _utils.colors.pictonBlue
+  },
+  secondaryTabWrapper: _extends({}, _utils.fonts.baseText, {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+    height: 80
+  }),
+  tabSecondaryStyle: {
+    flex: 1,
+    height: 70,
+    backgroundColor: _utils.colors.white,
+    alignSelf: 'center',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: 18,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    outline: 'none',
+    marginRight: 2,
+    color: _utils.colors.dustyGray,
+    ':hover': {
+      color: _utils.colors.white,
+      backgroundColor: _utils.colors.dustyGray,
+      height: 80,
+      paddingTop: 10,
+      marginTop: -10,
+      borderTopLeftRadius: 4,
+      borderTopRightRadius: 4
+    }
+  },
+  tabSecondaryStyleSelected: {
+    color: _utils.colors.white,
+    backgroundColor: _utils.colors.dustyGray,
+    height: 80,
+    paddingTop: 10,
+    marginTop: -10,
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 4
   }
+
 };
